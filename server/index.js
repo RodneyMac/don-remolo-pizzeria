0ea68d9b21230c0a7436b5fd24a3d2e20441f1d5
ArceDaniel <jonathandanielarce9@gmail.com>
2022-10-10 23:41:32 -0300

    database creation

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
-console.clear();
+
 import * as dotenv from 'dotenv';
 import Express from 'express';
+import taskRouter from './routers/task.routers.js';
+import morgan from 'morgan';
+import {path} from 'path';
+
+//initialize server
+PORT = process.env.PORT || 3000
 
 dotenv.config()
 const app = Express();
 
-app.use(Express.static('../public'))
+//Middlewares
+app.use(Express.json());
+app.use(morgan('dev'));
 
-app.listen(process.env.PORT,()=>{
-console.log(`server runing in the port ${process.env.PORT}`)
-});
+//routers
+app.use('/',taskRouter);
+app.use(Express.static(path.join(__dirname + '../public')));
+
+
+//starting server
+async function bootstraps() {
+    await mongoose.connect(process.env.URI);
+
+    app.listen(PORT, () => {
+        console.log(`estoy en el puerto ${PORT} ta todo flama rey`);
+
+    });
+}
+
+bootstraps();
